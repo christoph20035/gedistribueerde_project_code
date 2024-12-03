0eca57fc082e9057e313c3574b95ebf3326e1292
christoph20035 <46644563+christoph20035@users.noreply.github.com>
2024-12-03 10:31:26 +0100

    remove message file

--- Client.java
+++ Client.java
@@ -11,7 +11,6 @@ import java.rmi.registry.Registry;
 import java.security.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.List;
 
 public class Client {
